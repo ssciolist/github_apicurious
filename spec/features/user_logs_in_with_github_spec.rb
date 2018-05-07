bf35faab0b5af8790350354bf8e4d079b378163c
Megan Arellano <ssciolist@gmail.com>
2018-05-07 17:59:25 -0600

    adds first oauth test

---
+++ spec/features/user_logs_in_with_github_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'user logs in using Github' do
+  scenario 'they log in and see the homepage with their name' do
+    stub_omniauth
+    visit root_path
+    expect(page).to have_link('Login')
+    click_link 'Login'
+    expect(page).to have_content('ssciolist')
+    expect(page).to have_link('Logout')
+  end
+end
