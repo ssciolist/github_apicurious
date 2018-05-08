01dc1b73d91eeeba129b2953124511ad6caaf263
Megan Arellano <ssciolist@gmail.com>
2018-05-08 13:38:44 -0600

    fixes screen name display issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth_info)
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.uid = auth_info.uid
-      new_user.screen_name = auth_info.extra.raw_info.screen_name
+      new_user.screen_name = auth_info.info.nickname
       new_user.oauth_token = auth_info.credentials.token
       new_user.oauth_token_secret = auth_info.credentials.secret
     end
