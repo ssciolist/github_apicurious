3ecb507cfa20c2fdc28cb5b8ca6c36b91394cc88
Megan Arellano <ssciolist@gmail.com>
2018-05-08 17:00:50 -0600

    functional, but untested repository call method

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,9 +1,13 @@
 class Repository
-  attr_reader :all
+  attr_reader :all, :name, :url, :description, :language, :description, :language, :last_updated
   def initialize(attrs)
-
+    @name = attrs[:name]
+    @url = attrs[:html_url]
+    @description = attrs[:description]
+    @language = attrs[:language]
+    @last_updated = attrs[:updated_at]
   end
   def self.all
-    binding.pry
+    ObjectSpace.each_object(self).to_a
   end
 end
