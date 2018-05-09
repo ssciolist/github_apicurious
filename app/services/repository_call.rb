760e359000a5f3e0a8984b6683f5e63c1c577841
Megan Arellano <ssciolist@gmail.com>
2018-05-08 20:47:37 -0600

    moves repositorycall to different folder

--- app/services/repository_call.rb
+++ app/services/repository_call.rb
@@ -1,8 +1,16 @@
 class RepositoryCall
+  def initialize(current_user)
+    @current_user = current_user
+  end
+
   def repositories
     conn = Faraday.new(url: "https://api.github.com/user/repos")
-    conn.post do |req|
-      req.headers['Authorization'] = ENV['GITHUB_AUTHORIZATION_TOKEN']
+    response = conn.get do |req|
+      req.headers['Authorization'] = "token " + @current_user.oauth_token
+    end
+    raw_repos = JSON.parse(response.body, symbolize_names: true)
+    raw_repos.map do |raw_repo|
+      Repository.new(raw_repo)
     end
   end
 end
