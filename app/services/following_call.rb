8e86e6dc51e264430dd0849b0324d691c1bda79d
Megan Arellano <ssciolist@gmail.com>
2018-05-09 17:24:41 -0600

    messy followingcall class with follower method

---
+++ app/services/following_call.rb
@@ -0,0 +1,16 @@
+class FollowingCall
+  def initialize(current_user)
+    @current_user = current_user
+  end
+
+  def following
+    conn = Faraday.new(url: "https://api.github.com/users/#{@current_user.screen_name}/following")
+    response = conn.get do |req|
+      req.headers['Authorization'] = "token " + @current_user.oauth_token
+    end
+    raw_following = JSON.parse(response.body, symbolize_names: true)
+    raw_following.map do |raw_user|
+      FollowedUser.new(raw_user)
+    end
+  end
+end
