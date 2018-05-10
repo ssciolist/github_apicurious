b3655886ceff15458c16399351b173b3a3e372c7
Megan Arellano <ssciolist@gmail.com>
2018-05-10 10:03:26 -0600

    adds api call to gh star service

--- app/services/github_stars_service.rb
+++ app/services/github_stars_service.rb
@@ -3,12 +3,19 @@ class GithubStarsService
     @current_user = current_user
   end
 
-  def stars
+  def starred_repos
+    JSON.parse(raw_search, symbolize_names: true)
   end
 
   private
 
   def conn
+    Faraday.new(url: "https://api.github.com/users/#{@current_user.screen_name}/starred")
   end
-  
+
+  def raw_search
+    conn.get do |req|
+      req.headers['Authorization'] = "token " + @current_user.oauth_token
+    end
+  end
 end
