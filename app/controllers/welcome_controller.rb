e28a04e26d6417ce0a86db8aa3c9882c3a209098
Megan Arellano <ssciolist@gmail.com>
2018-05-09 20:02:58 -0600

    adds follower model test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,6 @@ class WelcomeController < ApplicationController
     elsif params[:tab] == "followers"
       FollowerCall.new(current_user).followers
       @followers = Follower.all
-
     elsif params[:tab] == "following"
       FollowingCall.new(current_user).following
       @following = FollowedUser.all
